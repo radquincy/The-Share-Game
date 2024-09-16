0c294ff9f8642c77597d4226ae1d74940159daf8
Rad_Quincy <darcy_oneill@icloud.com>
2024-09-16 12:34:07 +1000

    Update connect.php

--- extras/important/mws/connect.php
+++ extras/important/mws/connect.php
@@ -3,9 +3,9 @@ require('require_me.php');
 
 //conect to the database
 $host = "localhost";
-$dbname = "oneild01db";
-$usernameDB = "oneild_db";
-$passwordDB = "oneild_pass";
+$dbname = "******01db";
+$usernameDB = "******_db";
+$passwordDB = "******_pass";
 @$connect = mysqli_connect($host, $usernameDB, $passwordDB) or die("ERROR: Unable to connect to database");
 @mysqli_select_db($connect,$dbname) or die("ERROR: Unable to connect to database");
 
